5fbc1252cc1fe55d4fe0d7e82a5422afe063f670
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-11-02 11:51:11 +0000

    refactored code to reuse code

--- morsedecoder.rb
+++ morsedecoder.rb
@@ -11,36 +11,15 @@ def decode_char(character)
 end
 
 def decode_word(morse_code)
-  morse_code_dict = {
-      'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.', 
-      'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-',  'L' => '.-..', 
-      'M' => '--',  'N' => '-.',  'O' => '---',  'P' => '.--.', 'Q' => '--.-', 'R' => '.-.',  
-      'S' => '...',  'T' => '-',   'U' => '..-',  'V' => '...-', 'W' => '.--',  'X' => '-..-', 
-      'Y' => '-.--',  'Z' => '--..'
-  }
-  
-  # Reverse the dictionary to map morse code to letters
-  morse_code_dict = morse_code_dict.invert
-  
-  # Split the morse code by space and decode
-  return morse_code.split(' ').map { |code| morse_code_dict[code] }.join
+  morse_code.split.map { |char| decode_char(char) }.join
 end
 
 def decode_message(morse_code)
-  morse_code_dict = {
-      'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.', 
-      'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-',  'L' => '.-..', 
-      'M' => '--',  'N' => '-.',  'O' => '---',  'P' => '.--.', 'Q' => '--.-', 'R' => '.-.',  
-      'S' => '...',  'T' => '-',   'U' => '..-',  'V' => '...-', 'W' => '.--',  'X' => '-..-', 
-      'Y' => '-.--',  'Z' => '--..'
-  }
-  
-  # Reverse the dictionary to map morse code to letters
-  morse_code_dict = morse_code_dict.invert
-  
-  # Split the morse code by three spaces to get words
-  words = morse_code.split('   ')
-  
-  # Split each word by space to get letters and decode
-  return words.map { |word| word.split(' ').map { |code| morse_code_dict[code] }.join }.join(' ')
+  morse_code.strip.split('   ').map { |word| decode_word(word) }.join(' ')
 end
+character = '.-'
+puts decode_char(character)
+word = '-- -.--'
+puts decode_word(word)
+sentence = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+puts decode_message(sentence)
